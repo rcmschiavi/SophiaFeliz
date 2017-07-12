c42e8477b64df056c05c68134d142ec007f85cf5
Rodolfo Schiavi <rodolfocmschiavi@gmail.com>
2017-07-12 12:05:59 -0400

    Renovação e resposta funcionando
    A renovação está funcionando devidamente e já está sendo obtida a resposta da renovação

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 import Scraping
 #Livros para renovar, colocar os true or false no indice do livro a renovar
-livros_renovar=[True,False,False]
+livros_renovar=[True,True,False]
 Scraping.login()
 
 
 if len(Scraping.livros)>0:
     print(Scraping.usuario)
     print(Scraping.livros)
+    print(Scraping.id_livros)
 
-#função renovação está com bugs na parte de enviar o código dos livros a serem renovados e na vizualização da resposta da page
-#deve ser feita o encode do string para ser aceito no get
+
 Scraping.renovacao(livros_renovar)
--- Scraping.py
+++ Scraping.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-livros, prazos, usuario, data_urls, data_params = [], [], "", json, json
+livros, prazos, usuario, id_livros, data_urls, data_params = [], [], "", [], json, json
 c = requests.Session()
 
 #Função com os procedimentos de login
@@ -28,7 +28,7 @@ def login():
 
 #Função com os procedimentos de acesso às circulações
 def circ_op():
-    global data_urls, livros, prazos
+    global data_urls, livros, prazos, id_livros
 
     # Acessa a página de Circulações
     page_circ = c.get(data_urls['circ'])
@@ -41,29 +41,56 @@ def circ_op():
             tds = tr.findAll("td")
             livros.append(tds[2].text)
             prazos.append(tds[7].text)
+            for td in tds:
+                inps = td.findAll('input')
+                for inp in inps:
+                    id_livros.append(inp.get('value'))
+
     except:
         print("Erro na circulação")
 
 #Função que realiza as operações de renovação
-def renovacao(livros_renov = []):
-    lista = "num_circulacao : "
-    i=0
+def renovacao(livros_renov):
+    try:
+        #Define a variavel como o retorno da função de json
+        data_renov, lista = json_renov(livros_renov)
+        print(data_renov)
 
-    #Formata os indices de livros que serão renovados em indices para o get
+        #Faz o get enviando o parametro dos livros selecionados no url, no json do urls de renovação tem um {0}
+        #que possibilita a concatenação dos livros a serem renovados
+        page_renov = c.get(data_urls['renov'].format(lista), data=data_renov)
+
+        #Retira os resultados da resposta
+        soup_renov = BeautifulSoup(page_renov.content.decode('utf-8'), 'lxml')
+        trs = soup_renov.findAll("td", {"class": "td_tabelas_valor2 esquerda"})
+        num_livros =int(len(trs)/2 - 1)
+        for i in range(0,num_livros,1):
+            print(trs[3+i*2].text)
+
+    except: print("Erro na renovação")
+
+#Função que gera o json final para renovação
+def json_renov(livros_renov):
+    json_id_renov = {}
+    lista = []
+    i = 0
+    # Formata os indices de livros que serão renovados em indices para o get
     for itens in livros_renov:
-        if(itens):
-            lista+="71390{0}".format(i+1)
-        i+=1
+        if (itens):
+            lista.append(id_livros[i + 1])
+        i += 1
 
-    print(lista)
-    data_renov="{0}+{1}+{2}".format(data_params['renovacao_pt1'],lista,data_params['renovacao_pt2'])
-    print(data_renov)
-    page_renov = c.get(data_urls['renov'], data=data_renov)
-    soup_renov = BeautifulSoup(page_renov.text, 'lxml')
-    #Essa parte final ainda não está funcionando, para informar quais livros foram renovados com sucesso
-    #As vezes não dá pra renovar pq alguém reservou
-    trs = soup_renov.findAll("table", {"class": "tab_circulacoes max_width"}).findAll("tr")
-    for tr in trs:
-        tds = tr.findAll("td")
-        for td in tds:
-    print(page_renov.text)
+    #remove os caracteres indesejáveis para o concatenar o json
+    json_id_renov['num_circulacao'] = lista
+    json_id_renov = json.dumps(json_id_renov)
+    json_id_renov = json_id_renov.replace("{", "")
+    json_id_renov = json_id_renov.replace("}", "")
+    json_final_pt1 = json.dumps(data_params['renovacao_pt1']).replace("}", "")
+    json_final_pt2 = json.dumps(data_params['renovacao_pt2']).replace("{", "")
+    data_renov = "{0}, {1}, {2}".format(json_final_pt1, json_id_renov, json_final_pt2)
+    lista = str(lista)
+    lista = lista.replace("[","")
+    lista = lista.replace("]","")
+    lista = lista.replace("'","")
+    lista = lista.replace(" ","")
+    return data_renov, lista
